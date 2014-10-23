9e8d0c2dd6c585c9a126d9f9ebff81d03c805729
Kent Collins <kent.r.collins@gmail.com>
2014-10-23 20:56:13 +0100

    Update and rename Lab00.java to Lab03.java

--- Lab03.java
+++ Lab03.java
@@ -9,14 +9,9 @@
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
-public class Lab00 {
+public class Lab03 {
 
      public static void main(String[] args) {
-             Display.openWorld("maps/first.map");
-   Display.setSize(10, 10);
-   Display.setSpeed(3);
-   Robot karel = new Robot();
-   karel.move();
-   karel.move();
+          // TODO Your code goes here
      }
 }
